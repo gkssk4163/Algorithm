672097aab7fc3453a7c8d68916437ba027283ff7
hanna <gkssk4163@hanmail.net>
2021-11-25 22:42:38 +0900

    2021 Dev-Matching: 웹 백엔드 개발자(상반기) 문제풀이
    다단계 칫솔 판매

---
+++ Programmers/Dev_Matching_2021_웹_백엔드_개발자_상반기/다단계_칫솔_판매/Programmers_77486.java
@@ -0,0 +1,58 @@
+package Programmers.Dev_Matching_2021_웹_백엔드_개발자_상반기.다단계_칫솔_판매;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Programmers_77486 {
+	public static void main(String[] args) throws IOException {
+		Solution solution = new Solution();
+
+		String[] enroll1 = {"john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"};
+		String[] referral1 = {"-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"};
+		String[] seller1 = {"young", "john", "tod", "emily", "mary"};
+		int[] amount1 = {12, 4, 2, 5, 10};
+		System.out.println(Arrays.toString(solution.solution(enroll1, referral1, seller1, amount1)));
+
+		String[] enroll2 = {"john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"};
+		String[] referral2 = {"-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"};
+		String[] seller2 = {"sam", "emily", "jaimie", "edward"};
+		int[] amount2 = {2, 3, 5, 4};
+		System.out.println(Arrays.toString(solution.solution(enroll2, referral2, seller2, amount2)));
+	}
+}
+
+class Solution {
+	public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+		int[] answer = new int[enroll.length];
+
+		Map<String, String> relation = new HashMap<>();
+		Map<String, Integer> price = new HashMap<>();
+
+		for (int i = 0; i < enroll.length; i++) {
+			relation.put(enroll[i], referral[i]);
+			price.put(enroll[i], 0);
+		}
+
+		for (int i = 0; i < seller.length; i++) {
+			calculate(relation, price, seller[i], amount[i] * 100);
+		}
+
+		for (int i = 0; i < enroll.length; i++) {
+			answer[i] = price.get(enroll[i]);
+		}
+
+		return answer;
+	}
+
+	private void calculate(Map<String, String> relation, Map<String, Integer> price, String seller, int amount) {
+		while (relation.containsKey(seller)) {
+			int sellerAmount = (int) Math.ceil(amount * 0.9);
+			price.replace(seller, price.get(seller) + sellerAmount);
+
+			seller = relation.get(seller);
+			amount -= sellerAmount;
+		}
+	}
+}
