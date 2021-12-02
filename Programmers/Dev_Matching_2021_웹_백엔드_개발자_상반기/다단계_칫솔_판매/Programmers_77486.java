96c67a9bf1bc9acf99d7c36c2112d5e0fb8eadd5
hanna <gkssk4163@hanmail.net>
2021-12-02 22:00:13 +0900

    2021 Dev-Matching: 웹 백엔드 개발자(상반기) 문제풀이
    다단계 칫솔 판매
    시간초과 오류수정

--- Programmers/Dev_Matching_2021_웹_백엔드_개발자_상반기/다단계_칫솔_판매/Programmers_77486.java
+++ Programmers/Dev_Matching_2021_웹_백엔드_개발자_상반기/다단계_칫솔_판매/Programmers_77486.java
@@ -1,9 +1,7 @@
 package Programmers.Dev_Matching_2021_웹_백엔드_개발자_상반기.다단계_칫솔_판매;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Programmers_77486 {
 	public static void main(String[] args) throws IOException {
@@ -20,6 +18,12 @@ public class Programmers_77486 {
 		String[] seller2 = {"sam", "emily", "jaimie", "edward"};
 		int[] amount2 = {2, 3, 5, 4};
 		System.out.println(Arrays.toString(solution.solution(enroll2, referral2, seller2, amount2)));
+
+		String[] enroll3 = {"john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"};
+		String[] referral3 = {"-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"};
+		String[] seller3 = {"sam", "sam", "jaimie", "jaimie"};
+		int[] amount3 = {2, 3, 5, 4};
+		System.out.println(Arrays.toString(solution.solution(enroll3, referral3, seller3, amount3)));
 	}
 }
 
@@ -35,9 +39,7 @@ class Solution {
 			price.put(enroll[i], 0);
 		}
 
-		for (int i = 0; i < seller.length; i++) {
-			calculate(relation, price, seller[i], amount[i] * 100);
-		}
+		calculate(relation, price, parseLinkedHashMap(seller, amount));
 
 		for (int i = 0; i < enroll.length; i++) {
 			answer[i] = price.get(enroll[i]);
@@ -46,13 +48,43 @@ class Solution {
 		return answer;
 	}
 
-	private void calculate(Map<String, String> relation, Map<String, Integer> price, String seller, int amount) {
-		while (relation.containsKey(seller)) {
-			int sellerAmount = (int) Math.ceil(amount * 0.9);
-			price.replace(seller, price.get(seller) + sellerAmount);
+	private Map<String, LinkedList<Integer>> parseLinkedHashMap(String[] seller, int[] amount) {
+		Map<String, LinkedList<Integer>> sellerInfo = new LinkedHashMap<>();
+		for (int i = 0; i < seller.length; i++) {
+			if (!sellerInfo.containsKey(seller[i])) {
+				LinkedList<Integer> amountList = new LinkedList<>();
+				amountList.push(amount[i] * 100);
+				sellerInfo.put(seller[i], amountList);
+			} else {
+				sellerInfo.get(seller[i]).push(amount[i] * 100);
+			}
+		}
+		return sellerInfo;
+	}
 
-			seller = relation.get(seller);
-			amount -= sellerAmount;
+	private void calculate(Map<String, String> relation, Map<String, Integer> price, Map<String, LinkedList<Integer>> sellerInfo) {
+		while (!sellerInfo.isEmpty()) {
+			Map<String, LinkedList<Integer>> nextSellerInfo = new LinkedHashMap<>();
+			sellerInfo.forEach((seller, amountList) -> {
+				String referral = relation.get(seller);
+
+				amountList.forEach((amount) -> {
+					int fee = amount / 10;
+					price.replace(seller, price.get(seller) + amount - fee);
+
+					if (fee == 0) return;
+					if (!nextSellerInfo.containsKey(referral)) {
+						if (referral.equals("-")) return;
+
+						LinkedList<Integer> fees = new LinkedList<>();
+						fees.push(fee);
+						nextSellerInfo.put(referral, fees);
+					} else {
+						nextSellerInfo.get(referral).push(fee);
+					}
+				});
+			});
+			sellerInfo = nextSellerInfo;
 		}
 	}
 }
