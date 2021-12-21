e652834722661fcf81a4139598d57f5f01603c4d
hanna <gkssk4163@hanmail.net>
2021-12-21 23:12:47 +0900

    Greedy
    조이스틱 풀이 수정

--- Programmers/Greedy/조이스틱/Programmers_42860.java
+++ Programmers/Greedy/조이스틱/Programmers_42860.java
@@ -12,6 +12,12 @@ public class Programmers_42860 {
 
 		String name3 = "JAZ";
 		System.out.println(solution.solution(name3));
+
+		String name4 = "AAA";
+		System.out.println(solution.solution(name4));
+
+		String name5 = "ABAAAAAAAAABB";
+		System.out.println(solution.solution(name5));	// 7 : 실패
 	}
 }
 
@@ -19,14 +25,53 @@ class Solution {
 	public int solution(String name) {
 		int answer = 0;
 
-		for (int i = 0; i < name.length(); i++) {
-			char cur = name.charAt(i);
-			int move = Math.abs(cur - 'A');
-			int minMove = move <= 13 ? move : 26 - move;
-			answer += minMove;
-			if (i != name.length() - 1) answer += 1;
+		// 이름변경할 방향설정
+		int frontACount = 0;
+		int backACount = 0;
+
+		for (int i = 1; i <= Math.floor((name.length() - 1) / 2); i++) {
+			if (name.charAt(i) != 'A') break;
+			frontACount++;
 		}
 
-		return answer;
+		for (int i = (int) Math.ceil((double) name.length() / 2); i < name.length(); i++) {
+			if (name.charAt(i) != 'A') break;
+			backACount++;
+		}
+
+		boolean forward = frontACount <= backACount;
+
+		// 이름변경
+		final int MOVE = 1;
+		int lastACount = 0;
+		if (forward) {
+			answer += getAlphabetChangeCount(name, 0)  + MOVE;
+			for (int i = 1; i < name.length(); i++) {
+				answer += getAlphabetChangeCount(name, i);
+				if (i != name.length() - 1) answer += MOVE;
+
+				char cur = name.charAt(i);
+				if (cur == 'A') lastACount++;
+				else lastACount = 0;
+			}
+		} else {
+			answer += getAlphabetChangeCount(name, 0)  + MOVE;
+			for (int i = name.length() - 1; i > 0; i--) {
+				answer += getAlphabetChangeCount(name, i);
+				if (i != name.length() - 1) answer += 1;
+
+				char cur = name.charAt(i);
+				if (cur == 'A') lastACount++;
+				else lastACount = 0;
+			}
+		}
+
+		return answer - lastACount;
+	}
+
+	private int getAlphabetChangeCount(String name, int index) {
+		char cur = name.charAt(index);
+		int move = Math.abs(cur - 'A');
+		return move <= 13 ? move : 26 - move;
 	}
 }
